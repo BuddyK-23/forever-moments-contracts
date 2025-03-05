bd1ef7026f33cd5b42e7029e0ce14a53f747525c
BuddyK <buddyk@forevermoments.life>
2025-03-05 22:49:21 +0000

    Remove scripts folder from Git tracking

--- scripts/getHash.ts
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from "fs";
-import { ethers } from "ethers";
-
-const filePath = "./assets/profile-buddyk-barista.png"; // Replace with your actual file path
-const imageBuffer = readFileSync(filePath);
-
-const hash = ethers.keccak256(imageBuffer);
-console.log("Keccak-256 Hash:", hash);
